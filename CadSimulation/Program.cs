97cfa63f13b427e278a82df093b3538b9dab37a1
pscagliarini <p.scagliarini@vitamincenter.it>
2025-01-16 00:13:12 +0100

    Iterazione 1: aggiungere il supporto alla persistenza su file system dei dati, adottando il formato custom specificato

--- CadSimulation/Program.cs
+++ CadSimulation/Program.cs
@@ -3,6 +3,27 @@ using CadSimulation;
 
 List<Shape> shapes = new List<Shape>();
 
+static string? GetPathFromCommandLine(string[] commandLine)
+{
+  string? savePath = null;
+  if (commandLine.Length >= 2)
+  {
+    if (commandLine[0].Equals("--path", StringComparison.CurrentCultureIgnoreCase))
+    {
+      savePath = commandLine[1];
+    }
+  }
+  if (string.IsNullOrEmpty(savePath))
+  {
+    Console.WriteLine("Define saving path using parameter \"--path [path where to save]\".");
+  }
+  if (!Directory.Exists(Path.GetDirectoryName(savePath)))
+  {
+    Console.WriteLine("Saving path doesn't exists.");
+  }
+  return savePath;
+}
+
 while (true)
 {
   Console.WriteLine(
@@ -13,12 +34,16 @@ while (true)
 "   'r': insert a rectangle\n" +
 "   'l': list all inserted shapes\n" +
 "   'a': all shapres total area\n" +
+"----\n" +
+"   'k': save data\n" +
+"   'w': fetch data\n" +
 "   'q': quit");
 
   var k = Console.ReadKey(true);
   if (k.KeyChar == 'q')
     break;
 
+  string? savePath;
   Shape? shape = null;
   switch (k.KeyChar)
   {
@@ -39,16 +64,16 @@ while (true)
       break;
     case 'r':
       {
-        Console.WriteLine("Rectangle.\nValue for hight:\t");
+        Console.WriteLine("Rectangle.\nValue for height:\t");
         var hight = Int32.Parse(Console.ReadLine()!);
-        Console.WriteLine("value for weidth:\t");
+        Console.WriteLine("value for width:\t");
         var weidth = Int32.Parse(Console.ReadLine()!);
         shape = new Rectangle(hight, weidth); // Console.WriteLine("Rectangle");
       }
       break;
     case 't':
       {
-        Console.WriteLine("Triangle.\nValue for hight:\t");
+        Console.WriteLine("Triangle.\nValue for height:\t");
         var hight = Int32.Parse(Console.ReadLine()!);
         Console.WriteLine("value for base:\t");
         var weidth = Int32.Parse(Console.ReadLine()!);
@@ -69,9 +94,68 @@ while (true)
         Console.WriteLine("Total area: {0}", area);
       }
       continue;
+
+    case 'k': // save data
+      if (shapes.Count == 0)
+      {
+        Console.WriteLine("Nothing to save");
+        continue;
+      }
+      savePath = GetPathFromCommandLine(args);
+      if (!string.IsNullOrEmpty(savePath))
+      {
+        using (StreamWriter sw = new StreamWriter(savePath!))
+        {
+          foreach (Shape theShape in shapes)
+          {
+            sw.WriteLine(theShape.saveAsText());
+          }
+        }
+        Console.WriteLine("Data saved as text.");
+      }
+      continue;
+
+    case 'w': // fetch data
+      savePath = GetPathFromCommandLine(args);
+      if (!string.IsNullOrEmpty(savePath))
+      {
+        shapes.Clear();
+        using (StreamReader sr = new StreamReader(savePath))
+        {
+          while (!sr.EndOfStream)
+          {
+            string? rowData = sr.ReadLine();
+
+            Shape trySquare = new Square(rowData!);
+            if (trySquare.IsValid)
+              shapes.Add(trySquare);
+            else
+            {
+              Shape tryRectangle = new Rectangle(rowData!);
+              if (tryRectangle.IsValid)
+                shapes.Add(tryRectangle);
+              else
+              {
+                Shape tryCircle = new Circle(rowData!);
+                if (tryCircle.IsValid)
+                  shapes.Add(tryCircle);
+                else
+                {
+                  Shape tryTriangle = new Triangle(rowData!);
+                  if (tryTriangle.IsValid)
+                    shapes.Add(tryTriangle);
+                }
+              }
+            }
+          }
+        }
+
+        Console.WriteLine($"Data loaded. Found {shapes.Count} shapes.");
+        continue;
+      }
+      break;
   }
   shapes.Add(shape!);
-
 }
 
 namespace CadSimulation
@@ -80,13 +164,48 @@ namespace CadSimulation
   {
     void descr();
     double area();
+
+    /// <summary>
+    /// Save current data as text mode
+    /// </summary>
+    /// <returns></returns>
+    string saveAsText();
+    
+    /// <summary>
+    /// Return "true" is current shape data is valid
+    /// </summary>
+    bool IsValid { get; }
   }
   internal class Square : Shape
   {
     readonly int _side;
+    readonly bool _isValid;
+    bool Shape.IsValid { get { return _isValid; } }
+
     public Square(int side)
     {
       _side = side;
+      _isValid = true;
+    }
+
+    /// <summary>
+    /// Deserialize data and save information to shape properties. Extracts information and assigns it to properties
+    /// </summary>
+    /// <param name="serializeAsText">Serialized data from which to extract data</param>
+    public Square(string serializeAsText)
+    {
+      _isValid = false;
+      string[] values = serializeAsText.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+      if (values.Length == 2)
+      {
+        if (values[0] == "S")
+        {
+          if (int.TryParse(values[1], out _side))
+          {
+            _isValid = true;
+          }
+        }
+      }
     }
     double Shape.area()
     {
@@ -97,16 +216,47 @@ namespace CadSimulation
     {
       Console.WriteLine($"Square, side: {_side}");
     }
+    string Shape.saveAsText()
+    {
+      return $"S {_side}";
+    }
   }
   internal class Rectangle : Shape
   {
-    readonly int _height;
-    readonly int _width;
-    public Rectangle(int height, int weidth)
+    readonly private int _height;
+    readonly private int _width;
+    readonly bool _isValid;
+    bool Shape.IsValid { get { return _isValid; } }
+
+    public Rectangle(int height, int width)
     {
       _height = height;
-      _width = weidth;
+      _width = width;
+      _isValid = true;
     }
+
+    /// <summary>
+    /// Deserialize data and save information to shape properties. Extracts information and assigns it to properties
+    /// </summary>
+    /// <param name="serializeAsText">Serialized data from which to extract data</param>
+    public Rectangle(string serializeAsText)
+    {
+      _isValid = false;
+      string[] values = serializeAsText.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+      if (values.Length == 3)
+      {
+        if (values[0] == "R")
+        {
+          if (int.TryParse(values[1], out _height))
+          {
+            if (int.TryParse(values[2], out _width))
+              _isValid = true;
+          }
+        }
+      }
+    }
+
     double Shape.area()
     {
       return _height * _width;
@@ -114,15 +264,44 @@ namespace CadSimulation
 
     void Shape.descr()
     {
-      Console.WriteLine($"Rectangle, height: {_height}, weidth: {_width}");
+      Console.WriteLine($"Rectangle, height: {_height}, width: {_width}");
+    }
+    string Shape.saveAsText()
+    {
+      return $"R {_height} {_width}";
     }
   }
+
   internal class Circle : Shape
   {
-    int _radius;
+    readonly int _radius;
+    readonly bool _isValid;
+    bool Shape.IsValid { get { return _isValid; } }
+
     public Circle(int radius)
     {
       _radius = radius;
+      _isValid = true;
+    }
+
+    /// <summary>
+    /// Deserialize data and save information to shape properties. Extracts information and assigns it to properties
+    /// </summary>
+    /// <param name="serializeAsText">Serialized data from which to extract data</param>
+    public Circle(string serializeAsText)
+    {
+      _isValid = false;
+      string[] values = serializeAsText.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+      if (values.Length == 2)
+      {
+        if (values[0] == "C")
+        {
+          if (int.TryParse(values[1], out _radius))
+          {
+            _isValid = true;
+          }
+        }
+      }
     }
 
     double Shape.area()
@@ -134,16 +313,48 @@ namespace CadSimulation
     {
       Console.WriteLine($"Circle, radius: {_radius}");
     }
+    string Shape.saveAsText()
+    {
+      return $"C {_radius}";
+    }
   }
+
   internal class Triangle : Shape
   {
-    int _base;
-    int _height;
+    readonly int _base;
+    readonly int _height;
+    readonly bool _isValid;
+    bool Shape.IsValid { get { return _isValid; } }
+
     public Triangle(int b, int h)
     {
       _base = b;
       _height = h;
+      _isValid = true;
     }
+
+    /// <summary>
+    /// Deserialize data and save information to shape properties. Extracts information and assigns it to properties
+    /// </summary>
+    /// <param name="serializeAsText">Serialized data from which to extract data</param>
+    public Triangle(string serializeAsText)
+    {
+      _isValid = false;
+      string[] values = serializeAsText.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+      if (values.Length == 3)
+      {
+        if (values[0] == "T")
+        {
+          if (int.TryParse(values[1], out _base))
+          {
+            if (int.TryParse(values[2], out _height))
+              _isValid = true;
+          }
+        }
+      }
+    }
+
     double Shape.area()
     {
       return _base * _height / 2;
@@ -151,6 +362,10 @@ namespace CadSimulation
     void Shape.descr()
     {
       Console.WriteLine($"Triangle, base: {_base}, height: {_height}");
+    }
+    string Shape.saveAsText()
+    {
+      return $"T {_base} {_height}";
     }
   }
 }
